83831ccf0ab718273f9e35b64b050b7346a6b1ae
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-03-01 16:48:51 -0800

    Door Game v2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Gabriel Cballero
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
 
@@ -6,6 +7,42 @@ using namespace std;
 int main()
 {
   SecretDoor game;
+  int number;
+  int i;
+  int win = 0;
+  char letter;
+  game.newGame();
+  cout<<"How many times do you want to play the game?\n";
+  cin>>number;
+
+  for (i = 0; i < number; i++)
+    {
+     cout<<"Which door do you choose? (A/B/C)\n";
+     cin>>letter;
+     if (letter == 'a' || letter == 'A')
+        {
+         game.guessDoorC();
+        }
+     if (letter == 'h' || letter == 'H')
+        {
+         game.guessDoorC();
+        }
+         else
+            {
+             game.guessDoorC();
+            }
+            cout<<"Pick again for your second choice\n";
+            game.guessDoorC();
+            if (game.isWinner() == true)
+                {
+                 cout<<win++<<"You won?\n";
+                }
+                else
+                    {
+                     cout<<"You lose :(\n";
+                    }
+}     
+     cout<<"You won: "<<win<<endl;
     
   return 0;
 }
